02f93ee279853963149342ffd4271508e701eb4c
kumar1987an <kumar1987an@gmail.com>
2022-04-16 03:30:50 -0400

    .

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ Json_data = {
         "Jyothi Gadikal Krishnamurthy",
     ],
     "THE_DIRECTOR": "Subash, Ebenezer Joyson",
-    "HERO_TEAM": "APP Migration Factory Unix Team Members",
+    "HERO_TEAM": "Factory Unix Team Members",
     "EXECUTIVE_PRODUCER": "Schofield, Steve",
     "villain_team": ["Scrum Leads", "MMs", "MPMs", "Command Center"],
     "art_directors": (
@@ -35,9 +35,9 @@ ART_DIRECTORS = (j for j in Json_data["art_directors"])
 VILLAIN_TEAM = (k for k in Json_data["villain_team"])
 
 Film_Credits = f"""
-\t\t\t\t\t\t\t ***************
-\t\t\t\t\t\t\t * END CREDITS *
-\t\t\t\t\t\t\t ***************
+            ***************
+            * END CREDITS *
+            ***************
 
 DISCLAIMER: JUST FOR FUN NO HARD FEELINGS
 ^^^^^^^^^^
@@ -47,76 +47,81 @@ SCENE1 TAKE1 DATE: {Json_data['SCENE1_TAKE1_DATE'].strftime("%d-%B-%Y")}
 LAST DAY OF SHOOT: {Json_data['FINALSCENE_FINALTAKE_DATE'].strftime("%d-%B-%Y")}
 
 
-It's been an honour to work with this entire cast and crew have made the movie
-------------------------------------------------------------------------------
+It's been an honour to work with this entire 
+    cast and crew have made the movie
 
-\t\t\t\t\t\t\t   ============
-\t\t\t\t\t\t\t   | {Json_data['MOVIE_NAME']} |
-\t\t\t\t\t\t\t   ============
+            ============
+            | {Json_data['MOVIE_NAME']} |
+            ============
 
-\t\t\t\t\t\t\t {emoji.emojize(':dollar_banknote:')} a BlockBuster 
+          {emoji.emojize(':dollar_banknote:')} a BlockBuster 
 
- From the day 1 till date the makeover of the film is an awestruck process.
+From the day 1 till date the makeover of the 
+        film is an awestruck process.
 
-\t\t\tI would like to splash my special thanks to Producers
-\t\t\t--->--->--->--->--->--->--->--->--->--->--->--->--->-
+I would like to splash my special thanks to 
+        Fantabulous Producers
+--->--->--->--->--->--->--->--->--->--->----
 
-\t\t\t\t\t\t  {emoji.emojize(':partying_face:')} {next(PRODUCERS)}
+        {emoji.emojize(':partying_face:')} {next(PRODUCERS)}
 
-\t\t\t\t\t     {emoji.emojize(':face_with_monocle:')} {next(PRODUCERS)}
+        {emoji.emojize(':face_with_monocle:')} {next(PRODUCERS)}
 
-\t\t\t\t\t   {emoji.emojize(':flexed_biceps:')} {next(PRODUCERS)}
-
-
-\tWithout a captain there is no ship he is our multi-faceted Director
-\t---<---<---<---<---<---<---<---<---<---<---<---<---<---<---<---<---
-
-\t\t\t\t\t\t {emoji.emojize(':cinema:')} {Json_data["THE_DIRECTOR"]}
+        {emoji.emojize(':flexed_biceps:')} {next(PRODUCERS)}
 
 
-\t  Special Thanks to our proactive & stupendous Executive Producer 
-\t  ---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---
+Without a captain there is no ship he is our 
+          multi-faceted Director
+---<---<---<---<---<---<---<---<---<---<----
 
-\t\t\t\t\t\t\t {emoji.emojize(':briefcase:')} {Json_data["EXECUTIVE_PRODUCER"]}
-
-
-\t\t Making audience convinced to reality is always complex, 
-\t\t   Countless Thanks for my Curated Art Director(s) Team
-\t\t {emoji.emojize(':man_mage:')}--+---+---+---+---+---+---+---+---+---+---+---+---+--{emoji.emojize(':woman_mage:')}
-
-\t\t\t\t\t\t   {emoji.emojize(':woman_technologist:')} {next(ART_DIRECTORS)}
-
-\t\t\t\t\t\t   {emoji.emojize(':man_technologist:')} {next(ART_DIRECTORS)}
-
-\t\t\t\t\t\t   {emoji.emojize(':tiger:')} {next(ART_DIRECTORS)}
-
-\t\t\t\t\t\t   {emoji.emojize(':nut_and_bolt:')} {next(ART_DIRECTORS)}
-
-\t\t\t\t\t\t   {emoji.emojize(':chains:')} {next(ART_DIRECTORS)}
-
-\t\t\t\t\t\t   {emoji.emojize(':fire:')} {next(ART_DIRECTORS)}
-
-\t\t\t\t\t\t   {emoji.emojize(':puzzle_piece:')} {next(ART_DIRECTORS)}
+        {emoji.emojize(':cinema:')} {Json_data["THE_DIRECTOR"]}
 
 
-\t\t  Welcome to the World of our Monstrous Villain team, 
-\t\t   Thank you guys for keeping us nail biting all day
-\t\t -+---+---+---+---+---+---+---+---+---+---+---+---+---
+Special Credits to our proactive & stupendous 
+          Executive Producer 
+---+---+---+---+---+---+---+---+---+---+---+-
 
-\t\t\t\t\t\t  {emoji.emojize(':skull:')} {next(VILLAIN_TEAM)}
-
-\t\t\t\t\t\t  {emoji.emojize(':smiling_face_with_horns:')} {next(VILLAIN_TEAM)}
-
-\t\t\t\t\t\t  {emoji.emojize(':alien_monster:')} {next(VILLAIN_TEAM)}
-
-\t\t\t\t\t\t  {emoji.emojize(':vampire:')} {next(VILLAIN_TEAM)}
+        {emoji.emojize(':briefcase:')} {Json_data["EXECUTIVE_PRODUCER"]}
 
 
-\t\t\t Hero Team always Hero to their self & others, 
-\t\t\t I proudly present my Hero team
-\t\t\t <><><><><><><><><><><><><><><><><><><><><><>
+        Making audience convinced to 
+         reality is always complex.
+Infinite Thanks for my Curated Art Directors Team
+{emoji.emojize(':man_mage:')}--+---+---+---+---+---+---+---+---+---+---+---{emoji.emojize(':woman_mage:')}
 
-\t\t\t {emoji.emojize(':person_in_tuxedo:')} {Json_data["HERO_TEAM"]}
+         {emoji.emojize(':woman_technologist:')} {next(ART_DIRECTORS)}
+
+         {emoji.emojize(':man_technologist:')} {next(ART_DIRECTORS)}
+
+         {emoji.emojize(':tiger:')} {next(ART_DIRECTORS)}
+
+         {emoji.emojize(':nut_and_bolt:')} {next(ART_DIRECTORS)}
+
+         {emoji.emojize(':chains:')} {next(ART_DIRECTORS)}
+
+         {emoji.emojize(':fire:')} {next(ART_DIRECTORS)}
+
+         {emoji.emojize(':puzzle_piece:')} {next(ART_DIRECTORS)}
+
+
+Welcome to the World of our Monstrous Villain team, 
+Thank you guys for keeping us nail biting all day
++---+---+---+---+---+---+---+---+---+---+---+---+---
+
+         {emoji.emojize(':skull:')} {next(VILLAIN_TEAM)}
+
+         {emoji.emojize(':smiling_face_with_horns:')} {next(VILLAIN_TEAM)}
+
+         {emoji.emojize(':alien_monster:')} {next(VILLAIN_TEAM)}
+
+         {emoji.emojize(':vampire:')} {next(VILLAIN_TEAM)}
+
+
+Hero Team always Hero to their self & others, 
+       I proudly present my Hero team
+{emoji.emojize(':party_popper:') * 20}
+
+        {emoji.emojize(':person_in_tuxedo:')} {Json_data["HERO_TEAM"]}
 
 
 The entire journey of carving this action movie we faced lot of twists and turns.
